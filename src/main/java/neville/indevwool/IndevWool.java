683f7ea4cc0e61a9ce23a9c16ace8b8fefbc5cfe
Neville Pethani <nevillepeth@gmail.com>
2025-06-29 10:27:30 -0400

    Initial commit

---
+++ src/main/java/neville/indevwool/IndevWool.java
@@ -0,0 +1,20 @@
+package neville.indevwool;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class IndevWool implements ModInitializer {
+	public static final String MOD_ID = "indev-wool";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/neville/indevwool/IndevWoolClient.java
@@ -0,0 +1,10 @@
+package neville.indevwool;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class IndevWoolClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
